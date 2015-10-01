8e0a44007e65c117be4d521ec2b2ba1110c51d52
sfmqur <sammaruska@gmail.com>
2015-10-01 18:42:22 -0500

    fixed random method usage

--- public/javascripts/genePool.js
+++ public/javascripts/genePool.js
@@ -3,17 +3,16 @@
 /*
 vars to be created with object
 this.numGenes
-this.pool = new Array();
+this.pool
  */
 
 function GenePool(numGenes){ //to randomly generate a genePool
     this.numGenes = numGenes;
     this.pool = new Array(numGenes*2);
     for(var x = 0; x < this.pool.length; x++){
-        if((Math.random() * 2) + 1 == 0){
+        if(parseInt(Math.random() * 10) % 2 == 0){
             this.pool[x] = false;
-        }
-        if((Math.random() * 2) + 1 != 0){
+        }else{
             this.pool[x] = true;
         }
     }
@@ -34,17 +33,15 @@ no application, they are just convenient placeholders*/
     }
 
     this.pool = new Array(this.numGenes*2);
-    //todo: check if random is used correctly
     for(var x = 0;x <= this.numGenes; x++){
-        //todo: the pool always ends up all trues
-        if(Math.random() *2 == 0) {
+        if(parseInt(Math.random() * 10) % 2 == 0) {
             this.pool[index] = mother.pool[x];
             index++;
         }else{
             this.pool[index] = mother.pool[x+1];
             index++;
         }
-        if(Math.random() *2 == 0){
+        if(parseInt(Math.random() * 10) % 2 == 0){
             this.pool[index] = father.pool[x];
             index++;
         }else{
